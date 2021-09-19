4601cc5b487223fb62f78a5c9ffbc34de874b0f7
Thanabadee Techakasiwattana <bankthanabadee2007@icloud.com>
2021-09-19 21:34:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+//replace ****
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '****Your Email****',
+    pass: '****Your mail password****'
+  }
+})
+
+var mailOptions = {
+    from: '****Your email****',
+    to: '****Email to****',
+    subject: '****Subject****',
+    text: '****Text to sent****',
+    html: '****HTML Format****'
+}
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+})
